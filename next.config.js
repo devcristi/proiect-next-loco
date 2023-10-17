04e8d3f2851ac8b4382fe3d396a2633d58a5d5db
cristi <cristiandanielboabes@gmail.com>
2023-10-17 20:12:59 +0300

    samll edit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['devcristi.github.io'], // Replace 'username.github.io' with your GitHub Pages domain.
+    unoptimized: true,
   },
   basePath: process.env.NODE_ENV === 'production' ? '/proiect-next-loco' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/proiect-next-loco/' : '',
