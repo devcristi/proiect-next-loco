79570c436ffe236da1d37af0c6dc82f91a55a438
cristi <cristiandanielboabes@gmail.com>
2023-10-17 20:02:52 +0300

    asdda

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/proiect-next-loco' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/proiect-next-loco/' : '',
+  output: 'export',
 };
 
 module.exports = nextConfig;
